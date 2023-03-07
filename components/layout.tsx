2701e8cbe7a09ed437b58bd9934e4cfe7747156e
Milad Nekofar <milad@nekofar.com>
2023-03-07 14:16:19 +0330

    chore: improve `layout` component and `index` page

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,15 +11,25 @@ const Layout: React.FC<Props> = ({ title, children }) => {
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="My static blog" />
+        <meta name="description" content="My static site" />
       </Head>
-      <header>
-        <h1>My Static Blog</h1>
-      </header>
-      <main>{children}</main>
-      <footer>
-        <p>© {new Date().getFullYear()} My Static Blog</p>
-      </footer>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl">
+          <div className="flex flex-col">
+            <header className="relative mx-auto max-w-7xl py-24 px-6 sm:py-32 lg:px-8">
+              <h1 className="text-4xl font-bold tracking-tight md:text-5xl lg:text-6xl">
+                My Static Site
+              </h1>
+            </header>
+            <main>{children}</main>
+            <footer>
+              <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+                © {new Date().getFullYear()} My Static Site
+              </p>
+            </footer>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
+import Layout from '../components/layout'
 
 const Home: NextPage = () => {
   return (
     <>
-      <div>Welcome to my static site!</div>
+      <Layout title="My Static Site">
+        <div className="mt-6 max-w-3xl text-center text-xl">
+          <p>Welcome to my static site!</p>
+        </div>
+      </Layout>
     </>
   )
 }
