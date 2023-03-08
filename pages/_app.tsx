e1c699279ccd5f242e322580c60857c9959ca122
Milad Nekofar <milad@nekofar.com>
2023-03-08 20:52:07 +0330

    feat: change default font to `vazirmatn` using `next/font`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Vazirmatn } from 'next/font/google'
+import '../styles/globals.css'
+
+const vazirmatn = Vazirmatn({
+  subsets: ['latin', 'arabic'],
+  variable: '--font-vazirmatn',
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className={`${vazirmatn.variable} font-sans`}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
